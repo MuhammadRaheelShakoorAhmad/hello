bb076a2998a4911a94576103a3014c8a9d4e9336
Raheel <raheelrana148@gmail.com>
2021-11-10 09:27:30 +0500

    added practice2 file

---
+++ practice2.py
@@ -0,0 +1,13 @@
+# Making a calss for calculator
+
+class Calculator:
+    
+    def add(self,num1,num2):
+        return num1 + num2
+    
+    def minus(num1,num2):
+        return num2 - num2
+
+
+obj = Calculator()
+print(obj.add(2,3))
